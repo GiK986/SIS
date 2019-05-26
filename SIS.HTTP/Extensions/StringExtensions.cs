1fa2bf0c2d0b5c6167c6698ce560f3cb0bce31a4
g.kyosev <g.kyosev86@gmail.com>
2019-05-26 12:58:25 +0300

    Add StringExtensions

---
+++ SIS.HTTP/Extensions/StringExtensions.cs
@@ -0,0 +1,7 @@
+﻿namespace SIS.HTTP.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string Capitalize(this string text) => text[0].ToString().ToUpper() + text.Substring(1).ToLower();
+    }
+}
